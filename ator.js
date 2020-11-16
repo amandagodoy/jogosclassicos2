0afd425edd46753c2566d233deff8f804f0f54c3
amandagodoy <amandagodoylima@gmail.com>
2020-11-16 03:08:17 -0300

    commit

--- ator.js
+++ ator.js
@@ -1,16 +1,33 @@
-//códico do ator
+//código do ator
+
 let xAtor = 100;
 let yAtor = 366;
+let colisao = false;
+
 
 function mostraAtor(){
-    image(imagemDoAtor, xAtor, yAtor, 30, 30);
+  image(imagemDoAtor, xAtor, yAtor, 30, 30);
 }
 
 function movimentaAtor(){
-    if (keyIsDown(UP_ARROW)){
-      yAtor -= 3;
+  if (keyIsDown(UP_ARROW)){
+    yAtor -= 3;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    yAtor += 3;
+  }
+}
+
+function verificaColisao(){
+  //collideRectCircle(x1, y1, width1, height1, cx, cy, diameter)
+  for (let i = 0; i < imagemCarros.length; i++){
+    colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15)
+    if (colisao){
+      colidiu();
     }
-    if (keyIsDown(DOWN_ARROW)){
-      yAtor += 3;
-    }
+  }
+}
+
+function colidiu(){
+  yAtor = 366;
 }
