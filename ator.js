5ac6866999329e43c66a8d43497f407d77f5ba38
amandagodoy <amandagodoylima@gmail.com>
2020-11-17 03:33:41 -0300

    commit

--- ator.js
+++ ator.js
@@ -1,10 +1,8 @@
 //código do ator
-
-let xAtor = 100;
+let xAtor = 85;
 let yAtor = 366;
 let colisao = false;
 let meusPontos = 0;
-
 
 function mostraAtor(){
   image(imagemDoAtor, xAtor, yAtor, 30, 30);
@@ -15,7 +13,9 @@ function movimentaAtor(){
     yAtor -= 3;
   }
   if (keyIsDown(DOWN_ARROW)){
-    yAtor += 3;
+    if(podeSeMover()){
+      yAtor += 3;
+    }
   }
 }
 
@@ -25,6 +25,10 @@ function verificaColisao(){
     colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15)
     if (colisao){
       voltaAtorParaPosicaoInicial();
+      somDaColisao.play();
+      if (pontosMaiorQueZero()){
+        meusPontos -= 1;
+      }
     }
   }
 }
@@ -33,16 +37,25 @@ function voltaAtorParaPosicaoInicial(){
   yAtor = 366;
 }
 
-function incluiPontos() {
-  textAling(center);
+function incluiPontos(){
+  textAlign(CENTER);
   textSize(25);
-  Fill(color(255, 240, 60));
+  fill(color(255, 240, 60))
   text(meusPontos, width / 5, 27);
 }
 
 function marcaPonto(){
   if (yAtor < 15){
     meusPontos += 1;
+    somDoPonto.play();
     voltaAtorParaPosicaoInicial();
   }
+}
+
+function pontosMaiorQueZero(){
+  return meusPontos > 0;
+}
+
+function podeSeMover(){
+  return yAtor < 366;
 }
